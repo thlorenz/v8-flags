dfd3da3a24c38a8e4adeeaded412128f2831d9f6
Thorsten Lorenz <thlorenz@gmx.de>
2014-06-28 11:50:17 -0400

    rewriting macros for easier reuse of flag-definitions code

--- src/v8_flag_definitions.cc
+++ src/v8_flag_definitions.cc
@@ -10,11 +10,31 @@ namespace i = v8::internal;
 
 /* v8 flag-definitions.h not properly cleaning this one up */
 #undef DEFINE_float
+#undef DEFINE_args
+#undef FLAG
 #define DEFINE_bool(nam, def, cmt)   FLAG(Boolean, nam)
 #define DEFINE_int(nam, def, cmt)    FLAG(Integer, nam)
 #define DEFINE_float(nam, def, cmt)  FLAG( Number, nam)
 #define DEFINE_string(nam, def, cmt) FLAG(String, nam)
 #define DEFINE_implication(x, y)
+#define DEFINE_args(nam, def, cmt)
+
+#define FLAG_FULL(type, nam)                         \
+  NAN_METHOD(Get_##nam) {                            \
+    NanScope();                                      \
+    NanReturnValue(NanNew<v8::type>(i::FLAG_##nam)); \
+  }                                                  \
+                                                     \
+  NAN_METHOD(Set_##nam) {                            \
+    NanScope();                                      \
+    NanReturnValue(NanNew<v8::type>(i::FLAG_##nam)); \
+  }
+
+#define FLAG_READONLY(type, nam)                     \
+  NAN_METHOD(Get_##nam) {                            \
+    NanScope();                                      \
+    NanReturnValue(NanNew<v8::type>(i::FLAG_##nam)); \
+  }
 
 #if (NODE_VERSION_AT_LEAST(0, 10, 28))
   #include "v8_flag_definitions_0.10.h"
@@ -22,26 +42,24 @@ namespace i = v8::internal;
   #include "v8_flag_definitions_0.8.h"
 #endif
 
-#define FLAG(type, nam)                              \
-  NAN_METHOD(Get_##nam) {                            \
-    NanScope();                                      \
-    NanReturnValue(NanNew<v8::type>(i::FLAG_##nam)); \
-  }
 
-EXPOSE(FLAG)
-#ifdef ENABLE_DISASSEMBLER
-EXPOSE_DISASSEMBLER(FLAG)
-#endif
-
+#undef FLAG_FULL
+#undef FLAG_READONLY
 #undef FLAG
 #define S(x) #x
-#define FLAG(type, nam) \
+
+#define FLAG_FULL(type, nam)                                                                        \
+  exports->Set(NanNew<v8::String>(S(nam)), NanNew<v8::FunctionTemplate>(Get_##nam)->GetFunction()); \
+  exports->Set(NanNew<v8::String>("set_" S(nam)), NanNew<v8::FunctionTemplate>(Set_##nam)->GetFunction());
+
+#define FLAG_READONLY(type, nam) \
   exports->Set(NanNew<v8::String>(S(nam)), NanNew<v8::FunctionTemplate>(Get_##nam)->GetFunction());
 
 void init(v8::Handle<v8::Object> exports) {
-EXPOSE(FLAG)
-#ifdef ENABLE_DISASSEMBLER
-EXPOSE_DISASSEMBLER(FLAG)
+#if (NODE_VERSION_AT_LEAST(0, 10, 28))
+  #include "v8_flag_definitions_0.10.h"
+#elif (NODE_VERSION_AT_LEAST(0, 8, 26))
+  #include "v8_flag_definitions_0.8.h"
 #endif
 }
 
@@ -51,6 +69,7 @@ NODE_MODULE(v8_flag_definitions, init)
 #undef DEFINE_int
 #undef DEFINE_float
 #undef DEFINE_string
-#undef FLAG
+#undef FLAG_FULL
+#undef FLAG_READONLY
 #undef S
 #undef DEFINE_implication
